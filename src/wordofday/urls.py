3a2d0b78e4a4f9f6532009e2256212d8179816b8
Mary <spiltfish@aim.com>
2015-12-06 10:24:10 -0800

    Separating ConnectionLib from other engine code

--- src/wordofday/urls.py
+++ src/wordofday/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from views import TodaysWordView
+from WODapp.views import TodaysWordView
 
 
 urlpatterns = [
