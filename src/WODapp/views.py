0fcff882158e890e81393ab23842764696b1dffb
zhaoweiyuan <zhaoweiyuan9@gmail.com>
2015-12-06 15:36:16 -0800

    some ugly but functioning code coming in! Now the home page is filled with word, part of speech, meaning, example, comments, person who posted the comment.

--- src/WODapp/views.py
+++ src/WODapp/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import TemplateView
-from models import Word
+from models import Word,Comment
 from forms import AddCommentForm
 
 
@@ -12,4 +12,5 @@ class TodaysWordView(TemplateView):
         context = super(TodaysWordView, self).get_context_data(**kwargs)
         context['english_words'] = Word.objects.filter(language_id__language_text="English")
         context['japanese_words'] = Word.objects.filter(language_id__language_text="Japanese")
+        context['comments'] = Comment.objects.all()
         return context
