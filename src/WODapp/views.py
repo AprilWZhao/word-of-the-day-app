0fd5f1d6169bf2ffe6c7d53bc7465d89018eb659
zhaoweiyuan <zhaoweiyuan9@gmail.com>
2015-12-06 12:53:13 -0800

    nothing really changed

--- src/WODapp/views.py
+++ src/WODapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from models import Word
 
+
 # Create your views here.
 class TodaysWordView(TemplateView):
     template_name = 'todays_word.html'
