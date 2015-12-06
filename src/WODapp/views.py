0818c4d07840ab98a5c2261d7f6364030572e5c5
zhaoweiyuan <zhaoweiyuan9@gmail.com>
2015-12-06 14:49:03 -0800

    Fixed the committing problem

--- src/WODapp/views.py
+++ src/WODapp/views.py
@@ -12,3 +12,4 @@ class TodaysWordView(TemplateView):
         context = super(TodaysWordView, self).get_context_data(**kwargs)
         context['english_words'] = Word.objects.filter(language_id__language_text="English")
         context['japanese_words'] = Word.objects.filter(language_id__language_text="Japanese")
+        return context
