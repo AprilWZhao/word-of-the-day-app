33a26dbac043a4504275dd8383e144dc8ada7631
mlarrousse <a.spilt.fish@gmail.com>
2015-12-05 21:17:31 -0800

    Updating todays word template

--- src/WODapp/views.py
+++ src/WODapp/views.py
@@ -5,4 +5,8 @@ from models import Word
 # Create your views here.
 class TodaysWordView(TemplateView):
     template_name = 'todays_word.html'
-    word_list = Word
+
+    def get_context_data(self, **kwargs):
+        context = super(TodaysWordView, self).get_context_data(**kwargs)
+        context['english_words'] = Word.objects.filter(language_id__language_text="English")
+        context['japanese_words'] = Word.objects.filter(language_id__language_text="Japanese")
